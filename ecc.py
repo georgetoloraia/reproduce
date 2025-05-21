a5b4d040e47a19547ae45b223978043a8736ef2d
georgetoloraia <gtoloraia19@gmail.com>
2025-05-21 18:24:37 +0400

    modified: ecc.py plus_to_set = 1 # ამ ცვლადში ვინახავ თითო იტერაციაზე ორიგინალ ცნობადს, თან ყოველ იტერაციაზე გაიზრდება ერთით

--- ecc.py
+++ ecc.py
@@ -21,34 +21,42 @@ def read_target_x(filename):
 
 def worker(target_x_set, queue):
     while True:
-        private = randint(1, N)
-        public_key = Secp256k1.scalar_mult(private, Secp256k1.G)
-        real_pub_x = public_key.x
-        your_pub_x = (private * Gx) % P
+        plus_to_set = 1 # ამ ცვლადში ვინახავ თითო იტერაციაზე ორიგინალ ცნობადს, თან ყოველ იტერაციაზე გაიზრდება ერთით
+        for seted in target_x_set:
+            private = seted + plus_to_set
+            public_key = Secp256k1.scalar_mult(private, Secp256k1.G)
+            real_pub_x = public_key.x
+            your_pub_x = (private * Gx) % N
 
-        for i in target_x_set:
-            back_to_private = (N - P + i) % P
-            result = (i + your_pub_x) % P
-            finished = (back_to_private + result) % N
+            for i in target_x_set:
+                back_to_private = (real_pub_x * i) % P
+                result = (i * your_pub_x) % P
+                finished = (back_to_private * result) % N
 
-            # შემოწმება
-            try:
-                pub = PublicKey.from_valid_secret(back_to_private.to_bytes(32, 'big'))
-                x = pub.point()[0]
-                if x in target_x_set:
-                    queue.put(back_to_private)
-                
-                pub_sec = PublicKey.from_valid_secret(result.to_bytes(32, 'big'))
-                x_sec = pub_sec.point()[0]
-                if x_sec in target_x_set:
-                    queue.put(result)
+                # შემოწმება
+                try:
+                    pub = PublicKey.from_valid_secret(back_to_private.to_bytes(32, 'big'))
+                    x = pub.point()[0]
+                    if x in target_x_set:
+                        queue.put(back_to_private)
 
-                pub_trd = PublicKey.from_valid_secret(finished.to_bytes(32, 'big'))
-                x_trd = pub_trd.point()[0]
-                if x_trd in target_x_set:
-                    queue.put(finished)
-            except:
-                continue
+                    pub_sec = PublicKey.from_valid_secret(result.to_bytes(32, 'big'))
+                    x_sec = pub_sec.point()[0]
+                    if x_sec in target_x_set:
+                        queue.put(result)
+
+                    pub_trd = PublicKey.from_valid_secret(finished.to_bytes(32, 'big'))
+                    x_trd = pub_trd.point()[0]
+                    print(f"04{hex(pub_trd.point()[0])[2:].zfill(64)}{hex(pub_trd.point()[1])[2:].zfill(64)}")
+                    # print(x_trd)
+                    if x_trd in target_x_set:
+                        queue.put(finished)
+                except:
+                    print("continue")
+                    continue
+            # break
+        plus_to_set += 1
+        print(plus_to_set)
 
 def writer(queue):
     with open("found_keys.txt", "a") as out:
